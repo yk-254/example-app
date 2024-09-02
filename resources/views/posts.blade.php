47111f28c60e6b78c24b422288c2608197e1000b
yk <yek.soltani66@gmail.com>
2024-09-02 18:32:05 +0330

    adding posts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,19 +8,19 @@
     <body>
 
         <article>
-            <h1><a href="/post">My First Post</a></h1>
+            <h1><a href="/post/my-first-post">My First Post</a></h1>
             <p>
                 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
             </p>
         </article>
         <article>
-            <h1><a href="/post">My Second Post</a></h1>
+            <h1><a href="/post/my-second-post">My Second Post</a></h1>
             <p>
                 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
             </p>
         </article>
         <article>
-            <h1><a href="/post">My Third Post</a></h1>
+            <h1><a href="/post/my-third-post">My Third Post</a></h1>
             <p>
                 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
             </p>
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,12 @@ Route::get('/', function () {
 Route::get('/posts', function () {
     return view('posts');
 });
-Route::get('/post', function () {
-    return view('post');
-});
+Route::get('/post/{post}', function ($slug) {
+    $filePath = resource_path('posts/' . $slug . '.html');
+    if (file_exists($filePath)) {
+        $postContent = file_get_contents($filePath);
+        return view('post', ['post' => $postContent]);
+    } else {
+        return view('post', ['post' => 'File not found']);
+    }
+})->where('post','[A-z_\-]+');
