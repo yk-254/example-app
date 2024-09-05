f92fe86cb87646f999e2f8e0225237768bd18553
yk <yek.soltani66@gmail.com>
2024-09-05 12:52:46 +0330

    using blade

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,13 +8,13 @@
 </head>
 
 <body>
-    <?php foreach ($posts as $post): ?>
-    <article>
-        <a href="/post/<?= $post->slug ?>">
-        <h1> <?= $post->title ?> </h1>
-        <div> <?= $post->body ?> </div>
-    </article>
-    <?php endforeach; ?>
+    @foreach ($posts as $post)
+        <article>
+            <a href="/post/<?= $post->slug ?>">
+                <h1> <?= $post->title ?> </h1>
+                <div> {!! $post->body !!} </div>
+        </article>
+    @endforeach
 </body>
 
 </html>
