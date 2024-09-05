153255bfbe0621218f3ed9aefe38f16d35e6881e
yk <yek.soltani66@gmail.com>
2024-09-05 12:36:49 +0330

    add cache

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,22 +4,23 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 
-
 class Post
 {
-    public function __construct(public $title, public $date,public $body,public $slug)
+    public function __construct(public $title, public $date, public $body, public $slug)
     {
-        
     }
 
     public static function all()
     {
-        return collect(File::files(resource_path('posts')))
-        ->map(fn($file) => YamlFrontMatter::parseFile($file))
-        ->map(fn($content) => new Post($content->title, $content->date, $content->body(), $content->slug))->sortByDesc('date');
+        return cache()->rememberForever('posts.all', function () {
+            return collect(File::files(resource_path('posts')))
+                ->map(fn($file) => YamlFrontMatter::parseFile($file))
+                ->map(fn($content) => new Post($content->title, $content->date, $content->body(), $content->slug))
+                ->sortByDesc('date');
+        });
     }
     public static function find($slug)
     {
-        return static ::all()->firstWhere('slug', $slug);
+        return static::all()->firstWhere('slug', $slug);
     }
 }
