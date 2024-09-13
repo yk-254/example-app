10fc1e1ce974742a16779c772097a674774dd7d8
yk <yek.soltani66@gmail.com>
2024-09-13 22:11:55 +0330

    add id

--- database/migrations/2024_09_13_082633_create_posts_table.php
+++ database/migrations/2024_09_13_082633_create_posts_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             // $table->string('slug')->nullable()->unique();
-            // $table->id();
+            $table->id();
             $table->string('title');
             $table->text('body');
             // $table->timestamps();
